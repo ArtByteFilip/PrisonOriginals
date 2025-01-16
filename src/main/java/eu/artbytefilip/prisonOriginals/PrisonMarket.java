f9ae693f09bb7ea526f5ea10870e9f5c9971885f
ArtByteFilip <artbytefilip@protonmail.com>
2025-01-16 17:50:21 +0100

    Warning fix

--- src/main/java/eu/artbytefilip/prisonOriginals/PrisonMarket.java
+++ src/main/java/eu/artbytefilip/prisonOriginals/PrisonMarket.java
@@ -25,8 +25,7 @@ public class PrisonMarket implements Listener, CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
+        if (commandSender instanceof Player player) {
             SellAll(player);
         } else {
             commandSender.sendMessage("This command can by executed only by player.");
